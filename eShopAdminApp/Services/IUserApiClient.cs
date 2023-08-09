ea7d54975301cd0035991baa331801fc082e5809
HoangHa <dh51806426@student.stu.edu.vn>
2023-08-09 15:36:56 +0700

    admin app

---
+++ eShopAdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,11 @@
+﻿using eShop.ViewModels.Common;
+using eShop.ViewModels.System.Users;
+using System.Threading.Tasks;
+
+namespace eShopAdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
